b40527d1fdc1b56078a5c771cead573b495b8d6a
michab1332 <bonowiczmichal1332@gmail.com>
2026-05-11 20:23:07 +0200

    feat: google

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 const consent = useCookie('cookie-consent', { default: () => 'undefined' })
 
-// Inicjalizacja Google Analytics z obsługą Consent Mode
 useScriptGoogleAnalytics({
   id: 'G-N09G4D6SG2',
-  /* 
-    Advanced Consent Mode:
-    Skrypt ładuje się zawsze, ale wysyła tylko anonimowe sygnały (pings) 
-    dopóki analytics_storage nie zostanie ustawione na 'granted'.
-  */
-  options: {
-    consent: consent.value === 'accepted' ? 'granted' : 'denied'
+  defaultConsent: {
+    analytics_storage: consent.value === 'accepted' ? 'granted' : 'denied',
+    ad_storage: consent.value === 'accepted' ? 'granted' : 'denied',
+    ad_user_data: consent.value === 'accepted' ? 'granted' : 'denied',
+    ad_personalization: consent.value === 'accepted' ? 'granted' : 'denied'
   }
 })
 </script>
@@ -20,7 +17,6 @@ useScriptGoogleAnalytics({
     <NuxtPage />
   </NuxtLayout>
   
-  <!-- Globalny baner ciasteczek -->
   <SharedCookieBanner />
 </template>
 
